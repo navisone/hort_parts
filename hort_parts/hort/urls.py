84b85f847637f5fbd8a91621b2a36f0a1e38a69a
AlexGr33n <greentr33@gmail.com>
2021-09-23 17:33:16 +0300

    update +-

--- hort_parts/hort/urls.py
+++ hort_parts/hort/urls.py
@@ -4,5 +4,8 @@ app_name = 'hort'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.AboutUsView.as_view(), name='about'),
+    path('contact/', views.ContactsView.as_view(), name='contacts'),
+    path('warranty/', views.WarrantyView.as_view(), name='warranty'),
 
 ]
--- hort_parts/hort/views.py
+++ hort_parts/hort/views.py
@@ -7,3 +7,40 @@ from .models import *
 
 def index(request):
     return render(request, 'hort_parts/index.html')
+
+
+class NewsView(ListView):
+    model = Content
+    queryset = Content.objects.filter(category_id=1)
+    context_object_name = 'news_list'
+    paginate_by = 10
+    template_name = 'hort_parts/news/news_list.html'
+
+
+class NewsDetailView(DetailView):
+    model = Content
+    slug_field = 'alias'
+    context_object_name = 'news_detail'
+    template_name = 'hort_parts/news/news_detail.html'
+
+
+class AboutUsView(ListView):
+    model = Content
+    queryset = Content.objects.filter(category_id=2)
+    context_object_name = 'about_list'
+    template_name = 'hort_parts/about.html'
+
+
+class ContactsView(ListView):
+    model = Content
+    queryset = Content.objects.filter(category_id=3)
+    context_object_name = 'contacts_list'
+    template_name = 'hort_parts/contact.html'
+
+
+class WarrantyView(ListView):
+    model = Content
+    queryset = Content.objects.filter(category_id=4)
+    context_object_name = 'warranty_list'
+    template_name = 'hort_parts/warranty.html'
+
