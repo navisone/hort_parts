e986fd1f00aa84416aa644f734df30ef195d2fb5
AlexGr33n <greentr33@gmail.com>
2021-10-14 16:46:55 +0300

    update template

--- hort_parts/static/js/hort_old/hort-catalog.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('a[href^="#catalog"]').click(function(){
-        var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top}, 1200);
-        return false;
-    });
-});
-
