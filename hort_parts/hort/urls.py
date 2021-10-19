8b45927b40dfd21d86ce5861d487367ba5ae7003
AlexGr33n <greentr33@gmail.com>
2021-10-19 16:51:52 +0300

    update +-

--- hort_parts/hort/urls.py
+++ hort_parts/hort/urls.py
@@ -4,9 +4,8 @@ app_name = 'hort'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('news/', views.NewsView.as_view(), name='news'),
-    path('about/', views.AboutView.as_view(), name='about'),
-    path('contact/', views.ContactsView.as_view(), name='contacts'),
-    path('warranty/', views.WarrantyView.as_view(), name='warranty'),
+    path('<slug:category_slug>/', views.product_list, name='product_list'),
+    path('<slug:category_slug>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search/', views.Search.as_view(), name='search'),
 
 ]
--- hort_parts/hort/views.py
+++ hort_parts/hort/views.py
@@ -9,38 +9,30 @@ def index(request):
     return render(request, 'hort_parts/index.html')
 
 
-class NewsView(ListView):
-    model = Content
-    queryset = Content.objects.filter(category_id=1)
-    context_object_name = 'news_list'
-    paginate_by = 10
-    template_name = 'hort_parts/news/news_list.html'
+def product_list(request, category_slug):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    category = get_object_or_404(Category, url=category_slug)
+    products = products.filter(category=category)
+    context = {'category': category, 'categories': categories, 'products': products}
+    return render(request, 'hort_parts/product/product_list.html', context)
 
 
-class NewsDetailView(DetailView):
-    model = Content
-    slug_field = 'alias'
-    context_object_name = 'news_detail'
-    template_name = 'hort_parts/news/news_detail.html'
+def product_detail(request, category_slug, slug):
+    product = get_object_or_404(Product, slug=slug)
+    category = get_object_or_404(Category, url=category_slug)
+    context = {'category': category, 'product': product}
+    return render(request, 'hort_parts/product/product_detail.html', context)
 
 
-class AboutView(ListView):
-    model = Content
-    queryset = Content.objects.filter(category_id=2)
-    context_object_name = 'about_list'
-    template_name = 'hort_parts/about.html'
+class Search(ListView):
+    paginate_by = 15
 
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.request.GET.get("q"))
 
-class ContactsView(ListView):
-    model = Content
-    queryset = Content.objects.filter(category_id=3)
-    context_object_name = 'contacts_list'
-    template_name = 'hort_parts/contact.html'
-
-
-class WarrantyView(ListView):
-    model = Content
-    queryset = Content.objects.filter(category_id=4)
-    context_object_name = 'warranty_list'
-    template_name = 'hort_parts/warranty.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'q={self.request.GET.get("q")}&'
+        return context
 
