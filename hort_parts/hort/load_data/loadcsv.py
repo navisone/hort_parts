e39fc52446dfcf702fac85571dc0a28c68d193b0
AlexGr33n <greentr33@gmail.com>
2022-02-01 16:02:46 +0200

    update +-

--- hort_parts/hort/load_data/loadcsv.py
+++ hort_parts/hort/load_data/loadcsv.py
@@ -11,7 +11,7 @@ class LoadData:
         session.auth = HTTPBasicAuth('Robot', 'Robot')
         transport = Transport(session=session, timeout=600)
         settings = Settings(xml_huge_tree=True)
-        self.client = Client('http://192.168.75.104/live/ws/navis_hort?wsdl',
+        self.client = Client('http://192.168.75.45/live/ws/navis_hort?wsdl',
                              transport=transport, settings=settings)
 
     def load_products(self):
@@ -56,7 +56,7 @@ class LoadData:
         file.write(str(data.decode('utf-8')))
         file.close()
 
-    def load_category_product_hort(self):
+    def load_category_product(self):
         deficit = self.client.service.GetData('category_product_hort')
         data = base64.b64decode(deficit)
         file = open('cache/category_product.csv', 'w', newline='', encoding='utf-8')
@@ -71,6 +71,6 @@ LoadData.load_description()
 LoadData.load_applicability()
 LoadData.load_product_manufacturer_model()
 LoadData.load_product_images()
-LoadData.load_category_product_hort()
+LoadData.load_category_product()
 
 print('Load Data Products')
